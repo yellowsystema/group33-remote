bec7e5f6064c4987df7ec2cf2d519d19c7cfd73e
Sato <kasato@ieng6-201.ucsd.edu>
2025-07-09 13:33:21 -0700

    bytespercp

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,6 +4,61 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+int byte_at_codepoint(char input[], size_t length) {
+	if (length == 0) {
+		return 0;
+	}
+	
+	unsigned char c = input[0];
+	int num_bytes = 0;
+
+	if (c < 128) {
+		num_bytes = 1;
+	} else if (( c & 0xE0) == 0xC0) {
+		num_bytes = 2;
+	} else if ((c & 0xF0) == 0xE0) {
+		num_bytes = 3;
+	} else if ((c & 0xF8) == 0xF0) {
+		num_bytes = 4;
+	} else {
+		return -1;
+	}
+
+	if (length < num_bytes) {
+		return -1;
+	}
+
+	for (int i = 1; i < num_bytes; i++) {
+		if ((input[i] & 0xC0) != 0x80) {
+			return -1;
+		}
+	}
+	return num_bytes;
+}
+
+int print_byte_as_cp(char str[]) {
+	int len = strlen(str);
+	bool is_first = true;
+	printf("Bytes per codepoint:");
+	for (int i = 0; i < len; ) {
+		int char_len = byte_at_codepoint(&str[i], len - i);
+		if (len <= 0) {
+			if (len < 0) {
+				return 1;
+			}
+				break;
+		}
+		if (!is_first) {
+			printf(" ");
+		}
+		is_first = false;
+		printf("%d", char_len);
+		i += char_len;
+	}
+		printf("\n");
+		return 0;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 	       	fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
@@ -60,3 +115,11 @@ int num_codepoints (char *str){
     return num;
 }
 
+<<<<<<< Updated upstream
+=======
+    // implement the UTF-8 analyzer here  
+    return print_byte_as_cp(argv[1]);
+}
+
+
+>>>>>>> Stashed changes
