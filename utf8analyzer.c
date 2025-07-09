da92241da631de4912dbd286a3692e49a9fcd288
Louie <jelouie@ieng6-201.ucsd.edu>
2025-07-09 12:58:09 -0700

    first two functions and print statements added

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,11 +4,38 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
-        return 1;
-    }
+bool is_valid_ascii(unsigned char *str) {
+	int index = 0;
+	while (str[index] != 0){
+		if (str[index] > 127){
+			return false;
+	     	}
+		index++;
+	}
+	return true;
+}
 
-    // implement the UTF-8 analyzer here
+void lowercase_to_uppercase_ascii(unsigned char *str){
+	int intdex = 0;
+	while(str[index] != 0){
+		if (str[index] >= 'a' && str[index] <= 'z'){
+			printf("%c", str[index] - 32);
+		} else {
+			printf("%c", str[index]);											}
+		index++;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+	       	fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
+		return 1;
+	}
+	
+	unsigned char *input = (unsigned char *)argv[1];
+	
+	printf("Valid ASCII: %s\n", is_valid_ascii(input) ? "Yes" : "No");
+       	printf("Uppercase string: ");
+	lowercase_to_uppercase_ascii(input);
+	printf("\n");
 }
